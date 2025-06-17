2110f49f40f441c9535b33a76f4b4ec8733d70a0
AnishaDate1 <2005734@apps.nsd.org>
2025-06-17 20:39:39 +0000

    Made Main file

---
+++ Main.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame.startGUI();
+    }
+}
