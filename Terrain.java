12bcc5d89125661ac2355b81aaef3e16c871e6a3
AnishaDate1 <2005734@apps.nsd.org>
2025-06-17 22:10:48 +0000

    made paintcomponent in terrain

--- Terrain.java
+++ Terrain.java
@@ -1,3 +1,22 @@
 public class Terrain extends JFrame {
-    
+    private static final int width = 600;
+    private static final int height = 600;
+    private static final int scaile = 5;
+    private double[][] terrainGrid;
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int x = 0; x < terrainGrid.length; x++) {
+            for (int y = 0; y < terrainGrid[0].length; y++) {
+                g.setColor(getTerrainColor(terrainGrid[x][y]));
+                g.fillRect(x * scaile, y * scaile, scaile, scaile);
+            }
+        }
+    }
+
+    public PerlinPanel() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
+
 }
